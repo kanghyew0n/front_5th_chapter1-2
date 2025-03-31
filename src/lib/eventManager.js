a66b5e16ddadc62cc7f38d262d2d3fff4184a0f7
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-01 00:33:35 +0900

    feat: eventManager 기능 추가 (전역변수 수정 필요)

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,38 @@
-export function setupEventListeners(root) {}
+let _root;
 
-export function addEvent(element, eventType, handler) {}
+const eventHandlers = {};
 
-export function removeEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  if (!root) {
+    return;
+  }
+
+  _root = root;
+
+  Object.keys(eventHandlers).forEach((eventType) => {
+    Object.keys(eventHandlers[eventType]).forEach((selector) => {
+      const handler = eventHandlers[eventType][selector];
+      if (handler) {
+        root.addEventListener(eventType, handler);
+
+        return function cleanup() {
+          root.removeEventListener(eventType, handler);
+        };
+      }
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventHandlers[eventType]) {
+    eventHandlers[eventType] = {};
+  }
+  eventHandlers[eventType][element] = handler;
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (eventHandlers[eventType][element] === handler) {
+    _root.removeEventListener(eventType, handler);
+    eventHandlers[eventType][element] = {};
+  }
+}
