9a8e26254385945b8e4658b6951fc306fc718e42
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-31 22:48:09 +0900

    chore: vnode 배열 0 포함하도록 수정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,6 +2,8 @@ export function createVNode(type, props, ...children) {
   return {
     type,
     props,
-    children: children.flat(Infinity).filter(Boolean),
+    children: children
+      .flat(Infinity)
+      .filter((child) => child === 0 || Boolean(child)),
   };
 }
