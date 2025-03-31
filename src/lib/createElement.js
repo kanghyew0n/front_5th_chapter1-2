f88cdf1e17613955a53b10b69568f733a37224f9
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-31 23:18:15 +0900

    feat: createElement

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,46 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (
+    vNode === null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const $fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      const $child = createElement(child);
+      $fragment.appendChild($child);
+    });
+    return $fragment;
+  }
+
+  const { type, props, children } = vNode;
+
+  const $el = document.createElement(type);
+  if (props) {
+    console.log(props);
+    Object.entries(props).forEach(([name, value]) => {
+      if (name.toLowerCase() === "classname") {
+        return $el.setAttribute("class", value);
+      }
+      $el.setAttribute(name, value);
+    });
+  }
+
+  children.forEach((child) => {
+    const $child = createElement(child);
+    $el.appendChild($child);
+  });
+
+  return $el;
+}
+
+// function updateAttributes($el, props) {}
