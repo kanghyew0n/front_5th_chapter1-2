363f7834511be12469d04db31fb09abdfa97c288
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-02 02:16:15 +0900

    refactor

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -6,9 +6,6 @@ export function normalizeVNode(vNode) {
     return `${vNode}`;
   }
 
-  // type이 인자가 있을 거라곤 생각 못했는데...! 테스트 코드의 { children, ...props }에서 자꾸 에러가 났다.
-  // type에 UnorderedList 함수가 들어가고 UnorderedList는 인자를 가지고 있다.
-  // type은 element의 tagName 뿐만 아니라 component다.
   if (vNode.type && typeof vNode.type === "function") {
     const { type, props, children } = vNode;
     const result = type({ ...props, children });
