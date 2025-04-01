70fb8ba36d72b55f4f8c2a32e5090141b2656877
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-02 02:16:38 +0900

    feat: renderElement 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,7 +4,16 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  const normalizedVNode = normalizeVNode(vNode);
+
+  if (container.oldNode !== undefined) {
+    updateElement(container, normalizedVNode, container.oldNode);
+  } else {
+    const $el = createElement(normalizedVNode);
+    container.appendChild($el);
+  }
+
+  setupEventListeners(container);
+
+  container.oldNode = normalizedVNode;
 }
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,77 @@
-import { addEvent, removeEvent } from "./eventManager";
+import { addEvent, removeEvent, setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+function updateAttributes(target, originNewProps = {}, originOldProps = {}) {
+  // newProps 반영
+  for (const [key, value] of Object.entries(originNewProps || {})) {
+    if (originOldProps[key] === originNewProps[key]) continue;
+    if (key.startsWith("on") && typeof value === "function") {
+      return addEvent(target, key.replace("on", "").toLowerCase(), value);
+    }
+    if (key.startsWith("class") && typeof value === "string") {
+      return target.setAttribute("class", value);
+    }
+    target.setAttribute(key, value);
+  }
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  // oldProps에만 남은 속성 제거
+  for (const key of Object.keys(originOldProps || {})) {
+    if (originNewProps[key] !== undefined) continue;
+    removeEvent(
+      target,
+      key.replace("on", "").toLowerCase(),
+      originOldProps[key],
+    );
+    target.removeAttribute(key);
+  }
+}
+
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  if (!parentElement) {
+    return;
+  }
+
+  // oldNode
+  if (!newNode && oldNode) {
+    parentElement.removeChild(parentElement.children[index]);
+    return removeEvent(oldNode, "click", oldNode.props.onClick);
+  }
+  // newNode
+  if (newNode && !oldNode) {
+    const $el = createElement(newNode);
+    parentElement.appendChild($el);
+    setupEventListeners(parentElement);
+    return;
+  }
+  // 모두 text 타입
+  if (typeof newNode === "string" && typeof oldNode === "string") {
+    if (newNode === oldNode) {
+      return;
+    }
+    return parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.children[index],
+    );
+  }
+
+  // 태그이름만 다를 경우
+  if (newNode.type !== oldNode.type) {
+    return parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.children[index],
+    );
+  }
+  // 태그이름이 같을 경우
+  updateAttributes(parentElement.children[index], newNode.props, oldNode.props);
+
+  // 반복
+  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+  for (let i = 0; i < maxLength; i++) {
+    updateElement(
+      parentElement.children[i],
+      newNode.children[i],
+      oldNode.children[i],
+      i,
+    );
+  }
+}
