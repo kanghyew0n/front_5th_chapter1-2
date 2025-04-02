2f60b9801bde22da06d7fd9e3f7f0b1464cf08d9
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-03 02:05:08 +0900

    fix: 이벤트 수정

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,4 +1,4 @@
-const eventHandler = {};
+const eventHandler = new Map();
 
 export function setupEventListeners(root) {
   if (!root) {
@@ -9,23 +9,30 @@ export function setupEventListeners(root) {
     root.addEventListener(eventType, handleEvent);
   });
 }
-
 function handleEvent(event) {
   const { type, target } = event;
-  if (eventHandler[type] && eventHandler[type][target]) {
-    eventHandler[type][target](event);
+
+  if (!eventHandler[type]) return;
+
+  for (const [element, handler] of eventHandler[type].entries()) {
+    if (element === target || element.contains(target)) {
+      event.stopPropagation();
+      event.preventDefault();
+      handler(event);
+      break;
+    }
   }
 }
 
 export function addEvent(element, eventType, handler) {
   if (!eventHandler[eventType]) {
-    eventHandler[eventType] = {};
+    eventHandler[eventType] = new Map();
   }
-  eventHandler[eventType][element] = handler;
+  eventHandler[eventType].set(element, handler);
 }
 
-export function removeEvent(element, eventType, handler) {
-  if (eventHandler[eventType][element] === handler) {
-    delete eventHandler[eventType][element];
+export function removeEvent(element, eventType) {
+  if (eventHandler[eventType]) {
+    eventHandler[eventType].delete(element);
   }
 }
