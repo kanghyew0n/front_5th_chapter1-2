412cf53e95623ea53cfaadab970904772dc4c39b
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-04 00:30:58 +0900

    chore: 배포

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
     esbuild: {
       jsxFactory: "createVNode",
+    },
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-2/" : "/",
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          hash: resolve(__dirname, "index.hash.html"),
+          404: resolve(__dirname, "404.html"),
+        },
+      },
     },
     optimizeDeps: {
       esbuildOptions: {
